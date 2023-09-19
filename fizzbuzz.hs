95a9955fdecf3e55110e53e1dfed59049fb5b2ed
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-09-19 17:07:42 -0400

    --update fizzbuzz for IO

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -1,9 +1,10 @@
 getFizzBuzzOutput :: Int -> String
 getFizzBuzzOutput x
-    | x `mod` 3 == 0 && x `mod` 5 == 0 = show "FizzBuzz"
-    | x `mod` 3 == 0 = show "Fizz"
-    | x `mod` 5 == 0 = show "Buzz"
+    | x `mod` 3 == 0 && x `mod` 5 == 0 = "FizzBuzz"
+    | x `mod` 3 == 0 = "Fizz"
+    | x `mod` 5 == 0 = "Buzz"
     | otherwise = show x
 
-runFizzBuzz :: [String]
-runFizzBuzz = [getFizzBuzzOutput x | x <- [1..100]]
+main :: IO ()
+main = do
+  sequence_ [putStrLn (getFizzBuzzOutput x) | x <- [1..100]]
